ec76f7af66b314b72f9e400221a595de8aafb0fd
DudeBro249 <yoayush@gmail.com>
2021-02-15 06:35:53 +0530

    library now uses basemodel for all fauna crud functions

--- fauna-forest/src/index.ts
+++ fauna-forest/src/index.ts
@@ -1,95 +1,15 @@
-import faunadb, { query as q } from 'faunadb'
 import { BaseModel } from './models/baseModel'
 import { FaunaDocument } from './models/faunaDocument'
+import { FaunaForestStore } from './store'
 
 interface ForestClientArgs {
-    client: faunadb.Client
+    faunaSecret: string
 }
 
-interface QueryIndexOptions {
-    getData: boolean
+const use = (clientArgs: ForestClientArgs) => {
+    new FaunaForestStore({
+        faunaSecret: clientArgs.faunaSecret
+    })
 }
 
-class ForestClient {
-    private clientArgs: ForestClientArgs
-    constructor(clientArgs: ForestClientArgs) {
-        this.clientArgs = clientArgs
-    }
-
-    async create<T>(baseModel: BaseModel, doc: T, id?: string): Promise<FaunaDocument<T>> {
-        const data = await baseModel.schema.validate(doc)
-        const createdDocument: FaunaDocument<T> = await this.clientArgs.client.query(
-            q.Create(
-                id != null ? q.Ref(
-                    q.Collection(baseModel.collection),
-                    id,
-                ) : q.Collection(baseModel.collection),
-                { data },
-            )
-        )
-        return createdDocument
-    }
-
-    async delete<T>(baseModel: BaseModel, id: string): Promise<FaunaDocument<T>> {
-        const deletedDocument: FaunaDocument<T> = await this.clientArgs.client.query(
-            q.Delete(
-                q.Ref(
-                    q.Collection(baseModel.collection),
-                    id
-                )
-            )
-        )
-
-        return deletedDocument
-    }
-
-    async update<T>(baseModel: BaseModel, doc: T, id: string): Promise<FaunaDocument<T>> {
-        const data = await baseModel.schema.validate(doc)
-
-        const updatedDocument: FaunaDocument<T> = await this.clientArgs.client.query(
-            q.Update(
-                q.Ref(
-                    q.Collection(baseModel.collection),
-                    id,
-                ),
-                { data },
-            )
-        )
-        return updatedDocument
-    }
-
-    async queryByIndex<T>(index: string, terms: any[], opts?: QueryIndexOptions): Promise<FaunaDocument<T>[] | [{ ref: any }]> {
-        let faunaQuery = q.Paginate(
-            q.Match(
-                q.Index(index),
-                terms.length > 1 ? terms : terms[0]
-            )
-        )
-        if (opts) {
-            if (opts.getData) {
-                faunaQuery = q.Map(
-                    faunaQuery,
-                    q.Lambda('X', q.Get(q.Var('X')))
-                )
-            }
-        }
-        const documents = await this.clientArgs.client.query(faunaQuery)
-        return documents as any
-    }
-    
-    async findById<T>(baseModel: BaseModel, id: string): Promise<FaunaDocument<T>> {
-        
-        const document: FaunaDocument<T> = await this.clientArgs.client.query(
-            q.Get(
-                q.Ref(
-                    q.Collection(baseModel.collection),
-                    id
-                )
-            )
-        )
-
-        return document
-    }
-}
-
-export { BaseModel, ForestClient, FaunaDocument }
+export { BaseModel, use, FaunaDocument }
--- tests/src/index.ts
+++ tests/src/index.ts
@@ -1,13 +1,12 @@
 import * as yup from 'yup'
 import * as forest from '../../fauna-forest'
-import faunadb from 'faunadb'
 
 const postSchema = yup.object().shape({
     title: yup.string().required(),
     content: yup.string().required()
 })
 
-const post = new forest.BaseModel('posts', postSchema)
+const Post = new forest.BaseModel('posts', postSchema)
 
 
 interface NewPost {
@@ -16,15 +15,12 @@ interface NewPost {
 }
 
 const main = async () => {
-    const faunaClient = new faunadb.Client({
-        secret: 'YOUR_CLIENT_SECRET'
-    })
-    const forestClient = new forest.ForestClient({
-        client: faunaClient,
+    forest.use({
+        faunaSecret: 'YOUR_CLIENT_SECRET',
     })
 
     try {
-        const createdDocument = await forestClient.create<NewPost>(post, {
+        const createdDocument = await Post.create<NewPost>({
             title: 'this is my ohoho post title',
             content: 'this is my ohoho post content'
         })
@@ -32,25 +28,25 @@ const main = async () => {
         console.log('document created')
         console.log(createdDocument.data)
     
-        const foundDocument = await forestClient.findById<NewPost>(post, (createdDocument as any).ref.id)
-        console.log('Found document')
-        console.log(foundDocument.data)
+        // const foundDocument = await forestClient.findById<NewPost>(post, (createdDocument as any).ref.id)
+        // console.log('Found document')
+        // console.log(foundDocument.data)
     
-        const deletedDocument = await forestClient.delete(post, (createdDocument as any).ref.id)
-        console.log('deleted document: ')
-        console.log(deletedDocument.data)
+        // const deletedDocument = await forestClient.delete(post, (createdDocument as any).ref.id)
+        // console.log('deleted document: ')
+        // console.log(deletedDocument.data)
 
-        const documentReferences = await forestClient.queryByIndex('posts_by_content', ['my first post content'], {
-            getData: true
-        })
-        console.log(documentReferences)
+        // const documentReferences = await forestClient.queryByIndex('posts_by_content', ['my first post content'], {
+        //     getData: true
+        // })
+        // console.log(documentReferences)
 
-        const updatedDocument = await forestClient.update<NewPost>(post, {
-            title: 'updated title',
-            content: 'updated content'
-        }, '290384136264221185')
+        // const updatedDocument = await forestClient.update<NewPost>(post, {
+        //     title: 'updated title',
+        //     content: 'updated content'
+        // }, '290384136264221185')
 
-        console.log(updatedDocument)
+        // console.log(updatedDocument)
     }
     catch (error) {
         console.log(error)
